fc2231c75259f67027c550b4849768ca001ca4cd
thetime50 <1004763777@qq.com>
2020-06-26 22:06:59 +0800

    back

--- assemblyscript-test/assembly/index.ts
+++ assemblyscript-test/assembly/index.ts
@@ -1,6 +1,10 @@
 // The entry file of your WebAssembly module.
 
-export function add(a: i32, b: i32): i32 {
+function add(a: i32, b: i32): i32 {
   // console.log("WebAssembly add",a,b)
   return a + b;
+}
+
+export {
+  add,
 }
