aa5fb94696c33d92384a3562d25850ab26f8596c
thetime50 <1004763777@qq.com>
2020-07-02 11:53:51 +0800

    还原github的assetsPublicPath配置

--- laboratory/config/index.js
+++ laboratory/config/index.js
@@ -43,9 +43,9 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    // assetsPublicPath: '/',
-    // assetsPublicPath: '/front-laboratory/laboratory/dist/',
-    assetsPublicPath: './',
+    // assetsPublicPath: '/',//这里用的是绝对路径的逻辑
+    assetsPublicPath: '/front-laboratory/laboratory/dist/',
+    // assetsPublicPath: path.resolve(__dirname, '../dist'),
 
     /**
      * Source Maps
