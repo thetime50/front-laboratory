dac7cdb95de9c7850503233a3cc1297e25d1c54f
thetime50 <1004763777@qq.com>
2020-07-05 23:15:32 +0800

    back 动画

--- vuecli4/vc4/src/components/statsAnimation/feedDetail.vue
+++ vuecli4/vc4/src/components/statsAnimation/feedDetail.vue
@@ -43,10 +43,10 @@ export default {
     },
     methods:{
         handleEnter(){
-
+            console.log('handleEnter')
         },
         handleAfterEnter(){
-
+            console.log('handleAfterEnter')
         },
         handleFullStat(){
             console.log("handleFullStat")
@@ -63,6 +63,68 @@ export default {
     right: 0;
     bottom: 0;
     padding-top: 88px;
-    // .feed{}
+
+    ::v-deep{
+        .component-feed {
+            padding: 16px 20px 0;
+            transform: translate3d(0, 0, 0);
+        }
+        .feed_avatar {
+            transform: translate3d(-20px, -275px, 0);
+        }
+        .feed_text {
+            transform: translate3d(0, 0, 0);
+            opacity: 1;
+        }
+    }
+
+    // .feed-detail__stat ::v-deep{
+    //     background-color: #f1f2fb;
+    //     .post-list > ul > li {
+    //         perspective: 1900px;//透视
+    //     }
+    //     .post {
+    //         transform: translate3d(0, -50px, 0) rotateX(90deg);
+    //         transform-style: preserve-3d;
+    //         transition: all 0.6s ease-in;
+    //     }
+    //     .post-list {
+    //         opacity: 0;
+    //         transition: all 0.6s ease-in;
+    //     }
+    //     .feed_cover {
+    //         opacity: 0;
+    //     }
+    //     .feed-meta {
+    //         opacity: 0;
+    //     }
+    // }
+    //组件动画过程
+    &.show-leave-to{
+        transform: translate3d(0, 100% 0);
+    }
+    //https://segmentfault.com/q/1010000020658004/
+    &.show-enter ::v-deep{
+        .component-feed{
+            padding: 16px 8px 0;
+            transform: translate3d(0, 53px, 0);
+        }
+        .feed_avatar{
+            transform: translate3d(-82vw, -2vw, 0);
+        }
+        .feed_text {
+            transform: translate3d(100%, 0, 0);
+            opacity: 0;
+        }
+    }
+    &.show-enter-active ::v-deep,
+    &.show-leave-active ::v-deep{
+        transition: all 0.6s ease;
+        .component-feed,
+        .feed_avatar,
+        .feed_text{
+            transition: all 0.6s ease;
+        }
+    }
 }
 </style>
