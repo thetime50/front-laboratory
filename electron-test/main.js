e7c4d75b220cec150675c72687e7777de41275b8
thetime50 <1004763777@qq.com>
2020-07-07 21:49:09 +0800

    back

--- electron-test/main.js
+++ electron-test/main.js
@@ -43,11 +43,6 @@ function createWindow () {
     win.webContents.openDevTools()
   }
   
-  // Electron会在初始化完成并且准备好创建浏览器窗口时调用这个方法
-  // 部分 API 在 ready 事件触发后才能使用。
-  app.whenReady().then(createWindow)
-  
-  
 // --mem-pressure-system-reserved-kb
 // --args --js-flags="--max_old_space_size=8192"
 // --disk-cache-size 6000000
@@ -57,6 +52,13 @@ function createWindow () {
   // app.commandLine.appendSwitch('disk-cache-size', 6000000)
   // app.commandLine.appendArgument('disk-cache-size', 6000000)
   // app.commandLine.appendSwitch('js-flags', '--max_old_space_size=600')
+  app.commandLine.appendSwitch('disk-cache-size', '6000000')
+  // app.commandLine.appendArgument('disk-cache-size', '6000000')
+
+  // Electron会在初始化完成并且准备好创建浏览器窗口时调用这个方法
+  // 部分 API 在 ready 事件触发后才能使用。
+  app.whenReady().then(createWindow)
+  
 
   //当所有窗口都被关闭后退出
   app.on('window-all-closed', () => {
