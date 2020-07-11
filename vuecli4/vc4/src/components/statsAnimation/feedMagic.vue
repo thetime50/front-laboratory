634bd78179405b1fb70b32ac51601f4447d68abc
thetime50 <1004763777@qq.com>
2020-07-11 23:30:21 +0800

    初步的feedMagic.vue

--- vuecli4/vc4/src/components/statsAnimation/feedMagic.vue
+++ vuecli4/vc4/src/components/statsAnimation/feedMagic.vue
@@ -1,26 +1,42 @@
 <template>
-<div class="component-feed-magic">
-    feed-magic
-</div>
+<canvas class="component-feed-magic" />
 </template>
 
 <script>
 /* message */
+import {createRenderer} from "../../helper/wasm-canvas.js"
 
 export default {
     name: "feed-magic",
+    props:{
+        url:{type:String, required:true},
+        play:{type:Boolean},
+    },
     data () {
         return {
         };
     },
-    created(){
+    async mounted () {
+        this.$render = await createRenderer(this.$el, this.url) // nexttick?? maybe async delayed
+        this.$render()
         this.$emit('ready')
+    },
+    watch:{
+        play (val) {
+            if(val){
+                this.$render && this.$render(true)//paly
+            }
+        }
     },
 }
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
 .component-feed-magic{
-    
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
 }
 </style>
