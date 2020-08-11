b2d0ca1e56b0a5fafa91fb7da677a6fc13f3b8f2
TheTime50 <1004763777@qq.com>
2020-08-12 05:28:19 +0800

    caddy back

--- doc/backend/Caddyfile.sh
+++ doc/backend/Caddyfile.sh
@@ -1,16 +1,11 @@
 
 www.thetime50.com:80 {
     root /var/www/thetime50
+    ext .html
     tls off
-
-    proxy /rawgit https://cdn.rawgit.com {
-        header_upstream Host {host}
-        header_upstream X-Real-IP {remote}
-        header_upstream X-Forwarded-For {remote}
-        header_upstream X-Forwarded-Proto {scheme}
-  }
 }
 thetime50.com:80 {
     root /var/www/thetime50
+    ext .html
     tls off
 }
