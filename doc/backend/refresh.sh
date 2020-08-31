6ff480256b36f77b1c78e5dbb6cb94f417014329
thetime50 <1004763777@qq.com>
2020-08-31 13:16:45 +0800

    add git path front-server-laboratory back-laboratory

--- doc/backend/refresh.sh
+++ doc/backend/refresh.sh
@@ -15,6 +15,16 @@ git pull
 echo `pwd`
 echo -
 
+cd /home/zero/thetime50/front-server-laboratory
+git pull
+echo `pwd`
+echo -
+
+cd /home/zero/thetime50/back-laboratory
+git pull
+echo `pwd`
+echo -
+
 echo -
 echo -- $(date +%Y-%m-%d %H:%m:%s) --
 echo -
