1b373761e4b6daf1e4e22b2a627d291bb7f7a4d7
thetime50 <1004763777@qq.com>
2021-08-11 20:58:41 +0800

    proto back

--- laboratory/src/page/demo/protobuf/protobuf.vue
+++ laboratory/src/page/demo/protobuf/protobuf.vue
@@ -1,10 +1,15 @@
 <template>
     <div class="component-protobuf">
-        protobuf <br>
-        <a href="https://developers.google.com/protocol-buffers/">protocol-buffers</a> <br>
-        <a href="https://github.com/protocolbuffers/protobuf/tree/master/js">github google-protobuf</a> <br>
-        <a href="https://www.npmjs.com/package/google-protobuf">npm google-protobuf</a> <br>
-        <a href="https://github.com/protocolbuffers/protobuf/releases">Protocol Compiler</a>
+        <h2>protobuf</h2>
+        <div>
+            <a href="https://developers.google.com/protocol-buffers/">protocol-buffers</a> <br>
+            <a href="https://github.com/protocolbuffers/protobuf/tree/master/js">github google-protobuf</a> <br>
+            <a href="https://www.npmjs.com/package/google-protobuf">npm google-protobuf</a> <br>
+            <a href="https://github.com/protocolbuffers/protobuf/releases">Protocol Compiler</a>
+        </div>
+        <div>
+            <a href="https://segmentfault.com/a/1190000021052292">如何在前端中使用protobuf（vue篇）</a>
+        </div>
     </div>
 </template>
 
