9d27d114e46902a5efd964d4f816cb423d44c50b
thetime50 <1004763777@qq.com>
2022-06-08 02:26:36 +0800

    back

--- vue3-tsv/src/views/HomeView.vue
+++ vue3-tsv/src/views/HomeView.vue
@@ -2,6 +2,11 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+
+    <div>
+        <h2>页面路由</h2>
+        <router-link :to="{name: 'rubikCube'}">rubikCube</router-link>
+    </div>
   </div>
 </template>
 
