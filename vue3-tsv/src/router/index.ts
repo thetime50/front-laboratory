2646850f64ee41d89a7d9f7f797c1ac2e089c99f
xin_luo <xin_luo@fzzixun.com>
2023-02-14 23:25:53 +0800

    spline demo

--- vue3-tsv/src/router/index.ts
+++ vue3-tsv/src/router/index.ts
@@ -36,9 +36,14 @@ const routes: Array<RouteRecordRaw> = [
     component: () => import('../views/aStar/aStar.vue')
   },
   {
-      path: '/idaStar',
-      name: 'idaStar',
-      component: () => import('../views/idaStar/idaStar.vue')
+    path: '/idaStar',
+    name: 'idaStar',
+    component: () => import('../views/idaStar/idaStar.vue')
+  },
+  {
+      path: '/spline',
+      name: 'spline',
+      component: () => import('../views/splineView/splineView.vue')
   },
 ];
 
