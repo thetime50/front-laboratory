3db8aab0da94c3b03f386927fa49ce20807f04f9
thetime50 <1004763777@qq.com>
2026-08-28 19:18:01 +0800

    feat: 删除vue3-ts

--- vue3-ts/src/api/chart.ts
+++
@@ -1,4 +0,0 @@
-// import { http } from './http'
-import { graphDataMock } from './chartMock';
-
-export const graphData = () => ({ data: graphDataMock() }); // http.get('https://echarts.apache.org/examples/data/asset/data/webkit-dep.json',{})
